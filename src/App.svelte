9e520d442a7f62d3a792e620cd2be4cf2545fce9
swiftaff <swiftaff@gmail.com>
2022-09-17 19:56:10 +0800

    separate lines of text as inputs

--- src/App.svelte
+++ src/App.svelte
@@ -1,38 +1,31 @@
 <script>
-  import Greet from './lib/Greet.svelte'
+    import Greet from "./lib/Greet.svelte";
+    let rows = ["example", "text"];
+    let cursor = { row: 0, col: 7 };
+    function handleKeyPress(e) {
+        console.log(e.key);
+    }
 </script>
 
 <main class="container">
-  <h1>Welcome to Tauri!</h1>
+    <h1>Tauri Svelte Editor</h1>
 
-  <div class="row">
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
-    </a>
-    <a href="https://tauri.app" target="_blank">
-      <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank">
-      <img src="/svelte.svg" class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-
-  <p>
-    Click on the Tauri, Vite, and Svelte logos to learn more.
-  </p>
-
-  <div class="row">
-    <Greet />
-  </div>
-
+    {#each rows as row}
+        <input bind:value={row} on:keypress={handleKeyPress} />
+    {/each}
+    {JSON.stringify(rows)}
+    {JSON.stringify(cursor)}
+    <ul>
+        <li>separate lines of text as inputs - can't easily move up and down without createRange / setStart</li>
+    </ul>
 </main>
 
 <style>
-  .logo.vite:hover {
-    filter: drop-shadow(0 0 2em #747bff);
-  }
+    .logo.vite:hover {
+        filter: drop-shadow(0 0 2em #747bff);
+    }
 
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00);
-  }
+    .logo.svelte:hover {
+        filter: drop-shadow(0 0 2em #ff3e00);
+    }
 </style>
