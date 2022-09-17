a9606ed0a3f7d2b540f77c23c9d3ea430ee31323
swiftaff <swiftaff@gmail.com>
2022-09-17 21:01:33 +0800

    rows of characters using b, u, i for brevity

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,76 @@
 <script>
     import Greet from "./lib/Greet.svelte";
     let rows = ["example", "text"];
-    let cursor = { row: 0, col: 7 };
+    let cursor = { r: 0, c: 0 };
+    function caret(r, c) {
+        console.log("click", r, c);
+        if (r < rows.length) {
+            if (c > rows[r].length - 1) c = rows[r].length;
+            cursor = { r, c };
+        }
+    }
     function handleKeyPress(e) {
         console.log(e.key);
     }
 </script>
 
-<main class="container">
+<main class="container" on:keypress={handleKeyPress}>
     <h1>Tauri Svelte Editor</h1>
 
-    {#each rows as row}
-        <input bind:value={row} on:keypress={handleKeyPress} />
+    {#each rows as row, r}
+        <div on:mouseup={(e) => caret(r, 100000)}>
+            {r}: {#each [...row, ""] as char, c}{#if cursor.r == r && cursor.c == c}<i>.</i>{/if}<b
+                    style={"z-index:" + 100 + c}
+                    on:mouseup|stopPropagation={(e) => caret(r, c)}><u>.</u>{char}</b
+                >{/each}
+        </div>
     {/each}
     {JSON.stringify(rows)}
     {JSON.stringify(cursor)}
     <ul>
-        <li>separate lines of text as inputs - can't easily move up and down without createRange / setStart</li>
+        <li>rows of divs of characters = better text display, caret position selection and display</li>
     </ul>
 </main>
 
 <style>
-    .logo.vite:hover {
-        filter: drop-shadow(0 0 2em #747bff);
+    div {
+        user-select: none;
+        cursor: text;
     }
-
-    .logo.svelte:hover {
-        filter: drop-shadow(0 0 2em #ff3e00);
+    div:hover {
+        background-color: #ddd;
+    }
+    /* a character with a dot to the left for overlapping with adjacent-left character for ease of selection */
+    b {
+        font-weight: normal;
+        margin-left: -0.25em;
+        position: relative;
+    }
+    /* just the dot character inside the b above */
+    u {
+        opacity: 0;
+    }
+    /* flashing caret */
+    i {
+        font-weight: bold;
+        font-style: normal;
+        position: absolute;
+        margin-top: 2px;
+        height: 1.3rem;
+        margin-left: -0.05rem;
+        width: 3px;
+        background-color: green;
+        animation: flash 1s linear infinite;
+        z-index: 2000;
+    }
+    @keyframes flash {
+        0%,
+        50% {
+            opacity: 1;
+        }
+        51%,
+        100% {
+            opacity: 0;
+        }
     }
 </style>
