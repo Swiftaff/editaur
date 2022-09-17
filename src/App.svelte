e9538f833f86e5f265bd56f56b66d251c1111d4f
swiftaff <swiftaff@gmail.com>
2022-09-17 22:54:21 +0800

    global styles

--- src/App.svelte
+++ src/App.svelte
@@ -67,7 +67,7 @@
 
 <svelte:window on:keydown|preventDefault={handleKeyPress} />
 
-<main class="container">
+<main>
     test
     {#each rows as row, r}
         <div on:mouseup={(e) => caret_update(r, 100000)}>
@@ -81,71 +81,3 @@
         </div>
     {/each}
 </main>
-
-<style>
-    main {
-        border: 1px solid 666;
-        background-color: #eee;
-        min-height: calc(100vh - 80px);
-        padding: 5px;
-    }
-    div {
-        user-select: none;
-        cursor: text;
-        position: relative;
-        padding: 0px;
-        margin: 0px;
-        min-height: 1.5rem;
-    }
-    div:hover {
-        background-color: #ddd;
-    }
-    /* a character with a dot to the left for overlapping with adjacent-left character for ease of selection */
-    b {
-        font-weight: normal;
-        margin-left: -0.25em;
-        position: relative;
-        padding-top: 0.25rem;
-        padding-bottom: 0.25rem;
-    }
-    /* just the dot character inside the b above */
-    u {
-        opacity: 0;
-    }
-    /* flashing caret */
-    i {
-        font-weight: bold;
-        font-style: normal;
-        position: absolute;
-        margin-top: 0.02rem;
-        height: 1.5rem;
-        margin-left: -0.05rem;
-        width: 3px;
-        background-color: green;
-        animation: flash 1s linear infinite;
-        z-index: 2000;
-    }
-    @keyframes flash {
-        0%,
-        50% {
-            opacity: 1;
-        }
-        51%,
-        100% {
-            opacity: 0;
-        }
-    }
-    .text {
-        display: inline-block;
-        /* todo fix hard-coded width with flexbox layout? */
-        width: calc(100% - 150px);
-        margin-left: 100px;
-        position: relative;
-        top: 0px;
-    }
-    .num {
-        display: inline-block;
-        position: absolute;
-        top: 0px;
-    }
-</style>
