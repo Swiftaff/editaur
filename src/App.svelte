da179489d521b25544d1cbe3b6fbeaab194905a2
swiftaff <swiftaff@gmail.com>
2022-09-18 12:43:14 +0800

    scroll_up, scroll_down, rename functions snakecase

--- src/App.svelte
+++ src/App.svelte
@@ -5,24 +5,22 @@
     let cursor = { r: 0, c: 0 };
     let previous_c = 0;
     function caret_update(r, c) {
-        console.log("click", r, c);
+        //console.log("click", r, c);
         if (r < rows.length) {
             if (c > rows[r].length - 1) c = rows[r].length;
             cursor = { r, c };
         }
     }
     function handleKeyPress(e) {
-        console.log("key:", e.key);
-        if (e.key == "ArrowDown") arrowDown();
-        if (e.key == "ArrowUp") arrowUp();
-        if (e.key == "ArrowLeft") arrowLeft();
-        if (e.key == "ArrowRight") arrowRight();
+        //console.log("key:", e.key);
+        if (e.key == "ArrowDown") arrow_down();
+        if (e.key == "ArrowUp") arrow_up();
+        if (e.key == "ArrowLeft") arrow_left();
+        if (e.key == "ArrowRight") arrow_right();
     }
-    function arrowDown() {
+    function arrow_down() {
         let { r, c } = cursor;
-        let m = caret.clientTop;
-        let pos = caret.getBoundingClientRect();
-        console.log(m, pos);
+        scroll_down();
         if (r < rows.length - 1) {
             r = r + 1;
             c = move_caret_to_eol_if_shorter_than_previous(r, c);
@@ -32,11 +30,9 @@
         }
         cursor = { r, c };
     }
-    function arrowUp() {
+    function arrow_up() {
         let { r, c } = cursor;
-        let m = caret.clientTop;
-        let pos = caret.getBoundingClientRect();
-        console.log(m, pos);
+        scroll_up();
         if (r > 0) {
             r = r - 1;
             c = move_caret_to_eol_if_shorter_than_previous(r, c);
@@ -71,7 +67,7 @@
         }
         return c;
     }
-    function arrowLeft() {
+    function arrow_left() {
         let { r, c } = cursor;
         if (c > 0) {
             c = c - 1;
@@ -84,7 +80,7 @@
             cursor = { r, c };
         }
     }
-    function arrowRight() {
+    function arrow_right() {
         let { r, c } = cursor;
         if (c < rows[cursor.r].length) {
             c = c + 1;
@@ -97,12 +93,24 @@
             cursor = { r, c };
         }
     }
+    function scroll_up() {
+        let caret_position = caret.getBoundingClientRect();
+        if (caret_position.top < caret_position.height) {
+            main.scrollTo(0, main.scrollTop - caret_position.height);
+        }
+    }
+    function scroll_down() {
+        let caret_position = caret.getBoundingClientRect();
+        let main_position = main.getBoundingClientRect();
+        if (caret_position.top + caret_position.height > main_position.height) {
+            main.scrollTo(0, main.scrollTop + caret_position.height);
+        }
+    }
 </script>
 
 <svelte:window on:keydown|preventDefault={handleKeyPress} />
 
 <main bind:this={main}>
-    test
     {#each rows as row, r}
         <div on:mouseup={(e) => caret_update(r, 100000)} class={r == cursor.r ? "highlighted" : ""}>
             <span class="num" on:mouseup|stopPropagation={(e) => caret_update(r, 0)}>{1000 + r}: </span><span
