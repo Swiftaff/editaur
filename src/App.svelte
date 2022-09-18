64bfd2471c561331cad792a28a3819077d7c84f9
swiftaff <swiftaff@gmail.com>
2022-09-18 13:26:40 +0800

    mousewheel scrolling, with/without shift

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,8 @@
     let caret;
     let cursor = { r: 0, c: 0 };
     let previous_c = 0;
+    let pressing_shift = false;
+    const SHIFT_SCROLL_MULTIPLIER = 5;
     function caret_update(r, c) {
         //console.log("click", r, c);
         if (r < rows.length) {
@@ -11,12 +13,23 @@
             cursor = { r, c };
         }
     }
-    function handleKeyPress(e) {
+    function handle_key_down(e) {
         //console.log("key:", e.key);
         if (e.key == "ArrowDown") arrow_down();
         if (e.key == "ArrowUp") arrow_up();
         if (e.key == "ArrowLeft") arrow_left();
         if (e.key == "ArrowRight") arrow_right();
+        if (e.key == "Shift") shift_key_down();
+    }
+    function handle_key_up(e) {
+        //console.log("key:", e.key);
+        if (e.key == "Shift") shift_key_up();
+    }
+    function shift_key_down() {
+        if (!pressing_shift) pressing_shift = true;
+    }
+    function shift_key_up() {
+        pressing_shift = false;
     }
     function arrow_down() {
         let { r, c } = cursor;
@@ -93,22 +106,51 @@
             cursor = { r, c };
         }
     }
-    function scroll_up() {
+    function scroll_up(override = false) {
         let caret_position = caret.getBoundingClientRect();
-        if (caret_position.top < caret_position.height) {
-            main.scrollTo(0, main.scrollTop - caret_position.height);
+        console.log("scroll_up", caret_position, main.scrollTop);
+        if (caret_position.top < 0) {
+            console.log("TODO - mange scrolling when caret is offscreen");
+        }
+        if (override || caret_position.top < caret_position.height) {
+            main.scrollTo(
+                0,
+                main.scrollTop - caret_position.height * (override && pressing_shift ? SHIFT_SCROLL_MULTIPLIER : 1)
+            );
         }
     }
-    function scroll_down() {
+    function scroll_down(override = false) {
         let caret_position = caret.getBoundingClientRect();
         let main_position = main.getBoundingClientRect();
-        if (caret_position.top + caret_position.height > main_position.height) {
-            main.scrollTo(0, main.scrollTop + caret_position.height);
+        console.log("scroll_down", caret_position, main.scrollTop, main_position);
+        if (caret_position.top < 0) {
+            console.log("TODO - mange scrolling when caret is offscreen");
         }
+        if (override || caret_position.top + caret_position.height > main_position.height) {
+            main.scrollTo(
+                0,
+                main.scrollTop + caret_position.height * (override && pressing_shift ? SHIFT_SCROLL_MULTIPLIER : 1)
+            );
+        }
+    }
+    function wheel(node, _options) {
+        const handler = (e) => {
+            e.preventDefault();
+            if (e.deltaY < 0) scroll_up(true);
+            else scroll_down(true);
+        };
+
+        node.addEventListener("wheel", handler, { passive: false });
+
+        return {
+            destroy() {
+                node.removeEventListener("wheel", handler, { passive: false });
+            },
+        };
     }
 </script>
 
-<svelte:window on:keydown|preventDefault={handleKeyPress} />
+<svelte:window on:keydown|preventDefault={handle_key_down} on:keyup|preventDefault={handle_key_up} use:wheel={true} />
 
 <main bind:this={main}>
     {#each rows as row, r}
