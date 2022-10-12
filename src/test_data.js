a833e96e348cc47810495679880d43a481017eb3
swiftaff <swiftaff@gmail.com>
2022-10-12 23:56:42 +0800

    back to square one with character widths

--- src/test_data.js
+++ src/test_data.js
@@ -1,4 +1,6 @@
 export default [
+    "123456789 123456789 123456789 123456789 123456789",
+    "1234567890123456789012345678901234567890123456789",
     "example of really long text on a single line like you might see in a sentence with many words, much like this one. It might even go onto another line, or go on for thousands of characters in the case of a minified file.",
     "text",
     "text",
